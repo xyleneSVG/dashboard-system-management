20c470aed7135802029c47eec14261950ca331e5
xyleneSVG <dhavinf.a@gmail.com>
2026-03-25 13:02:23 +0700

    fix: sql

--- queries/get/project/issues.sql
+++ queries/get/project/issues.sql
@@ -1,5 +1,5 @@
 SELECT * FROM md_source.issues
-WHERE project_id = '${inputs.project_selected.value}'
+WHERE project_id = COALESCE('${inputs.project_selected.value}', project_id)
 AND CAST(to_timestamp(CAST(created_on AS BIGINT) / 1000) AS DATE)
 BETWEEN 
   COALESCE('${inputs.date_filter.start}', '1970-01-01')
