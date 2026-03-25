86e460eb9775b89210acced61cc4fa72bb081e58
xyleneSVG <dhavinf.a@gmail.com>
2026-03-25 12:45:45 +0700

    fix: default value date range picker

--- queries/get/project/issues.sql
+++ queries/get/project/issues.sql
@@ -1,4 +1,8 @@
 SELECT * FROM md_source.issues
 WHERE project_id = '${inputs.project_selected.value}'
-  AND CAST(to_timestamp(CAST(created_on AS BIGINT) / 1000) AS DATE) >= '${inputs.date_filter.start}' 
-  AND CAST(to_timestamp(CAST(created_on AS BIGINT) / 1000) AS DATE) <= '${inputs.date_filter.end}'
+  AND (
+    '${inputs.date_filter.start}' = '' 
+    OR '${inputs.date_filter.start}' IS NULL
+    OR CAST(to_timestamp(CAST(created_on AS BIGINT) / 1000) AS DATE) 
+       BETWEEN '${inputs.date_filter.start}' AND '${inputs.date_filter.end}'
+  )
