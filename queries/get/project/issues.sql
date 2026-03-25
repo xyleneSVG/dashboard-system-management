62d671e773eb15fbcde3a1f2aac0edb83603ec6c
xyleneSVG <dhavinf.a@gmail.com>
2026-03-25 13:10:57 +0700

    fix: sql

--- queries/get/project/issues.sql
+++ queries/get/project/issues.sql
@@ -1,6 +1,11 @@
-SELECT * FROM md_source.issues
-WHERE project_id = COALESCE('${inputs.project_selected.value}', project_id)
+SELECT * 
+FROM md_source.issues
+WHERE 
+  (
+    NULLIF('${inputs.project_selected.value}', '') IS NULL
+    OR project_id = '${inputs.project_selected.value}'
+  )
 AND CAST(to_timestamp(CAST(created_on AS BIGINT) / 1000) AS DATE)
 BETWEEN 
-  COALESCE('${inputs.date_filter.start}', '1970-01-01')
-  AND COALESCE('${inputs.date_filter.end}', CURRENT_DATE)
+  COALESCE(NULLIF('${inputs.date_filter.start}', ''), '1970-01-01')
+  AND COALESCE(NULLIF('${inputs.date_filter.end}', ''), CURRENT_DATE)
